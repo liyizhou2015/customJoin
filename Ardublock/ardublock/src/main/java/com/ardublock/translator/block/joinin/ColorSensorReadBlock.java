41778d8ef82e75ebbada88e4619df7cbea36f05d
lyz <404527001@qq.com>
2018-06-24 17:44:40 +0800

    mod colorSensor to tcs34725

--- Ardublock/ardublock/src/main/java/com/ardublock/translator/block/joinin/ColorSensorReadBlock.java
+++ Ardublock/ardublock/src/main/java/com/ardublock/translator/block/joinin/ColorSensorReadBlock.java
@@ -7,6 +7,31 @@ import com.ardublock.translator.block.exception.SubroutineNotDeclaredException;
 
 public class ColorSensorReadBlock extends TranslatorBlock
 {
+	private static final String CONFIG=""
+			+ "int getColorSensorRGB(int color){\n"
+			+ "uint16_t clear, red, green, blue;\n"
+			+ "tcs.setInterrupt(false);\n"
+			+ "tcs.getRawData(&red, &green, &blue, &clear);\n"
+			+ "tcs.setInterrupt(true);\n"
+			+ "delay(50);\n"
+			+ "tcs.setInterrupt(true);\n"
+			+ "uint32_t sum = clear;\n"
+			+ "float r, g, b;\n"
+			+ "r = red; r /= sum;\n"
+			+ "g = green; g /= sum;\n"
+			+ "b = blue; b /= sum;\n"
+			+ "r *= 256; g *= 256; b *= 256;\n"
+			+ "switch(color){\n"
+			+ "case 0:\n"
+			+ "  return int(r);\n"
+			+ "case 1:\n"
+			+ "  return int(g);\n"
+			+ "case 2:"
+			+ "  return int(b);\n"
+			+ "default:"
+			+ "  return 0;\n"
+			+ "}\n"
+			+ "}\n";
 	
 	public ColorSensorReadBlock(Long blockId, Translator translator, String codePrefix, String codeSuffix, String label)
 	{
@@ -18,8 +43,15 @@ public class ColorSensorReadBlock extends TranslatorBlock
 	{
 
 		TranslatorBlock tb = this.getRequiredTranslatorBlockAtSocket(0);
+
+		translator.addHeaderFile("Wire.h");
+		translator.addHeaderFile("Adafruit_TCS34725.h");
+		translator.addDefinitionCommand("Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);\n");
+		translator.addDefinitionCommand(CONFIG);
+		translator.addSetupCommand("tcs.begin();\n");
 		
-		String ret = "int( g_array[" + tb.toCode() + "] * g_SF[" + tb.toCode() + "] )";
+		String ret="";
+		ret += "getColorSensorRGB(" + tb.toCode() + ")";
 		
 		return codePrefix + ret + codeSuffix;
 	} 
