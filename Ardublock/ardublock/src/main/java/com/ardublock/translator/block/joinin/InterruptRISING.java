9b58192f1f18d0ff21c27bb83c619e0491f5c444
lyz <404527001@qq.com>
2019-03-03 13:02:51 +0800

    add interruptSetup block

---
+++ Ardublock/ardublock/src/main/java/com/ardublock/translator/block/joinin/InterruptRISING.java
@@ -0,0 +1,21 @@
+package com.ardublock.translator.block.joinin;
+
+import com.ardublock.translator.Translator;
+import com.ardublock.translator.block.TranslatorBlock;
+import com.ardublock.translator.block.exception.SocketNullException;
+import com.ardublock.translator.block.exception.SubroutineNotDeclaredException;
+
+public class InterruptRISING extends TranslatorBlock
+	{
+
+		public InterruptRISING(Long blockId, Translator translator, String codePrefix, String codeSuffix, String label)
+		{
+			super(blockId, translator, codePrefix, codeSuffix, label);
+		}
+
+		@Override
+		public String toCode() throws SocketNullException {
+			return codePrefix + "FALLING" + codeSuffix;
+		}
+		
+	}
---
+++ Ardublock/ardublock/src/main/java/com/ardublock/translator/block/joinin/InterruptSetup.java
@@ -0,0 +1,44 @@
+package com.ardublock.translator.block.joinin;
+import java.util.ResourceBundle;
+import com.ardublock.translator.Translator;
+import com.ardublock.translator.block.exception.BlockException;
+import com.ardublock.translator.block.exception.SocketNullException;
+import com.ardublock.translator.block.exception.SubroutineNotDeclaredException;
+import com.ardublock.translator.block.TranslatorBlock;
+import com.ardublock.translator.block.VariableNumberBlock;
+
+public class InterruptSetup extends TranslatorBlock
+{
+	
+	public InterruptSetup(Long blockId, Translator translator, String codePrefix, String codeSuffix, String label)
+	{
+		super(blockId, translator, codePrefix, codeSuffix, label);
+	}
+	private static ResourceBundle uiMessageBundle = ResourceBundle.getBundle("com/ardublock/block/ardublock");
+	
+
+	@Override
+	public String toCode() throws SocketNullException, SubroutineNotDeclaredException
+	{
+		
+		TranslatorBlock translatorBlock = this.getRequiredTranslatorBlockAtSocket(0);
+		String pin = translatorBlock.toCode();
+		translator.addSetupCommand("pinMode( " + pin + " , INPUT );\n");
+
+		String mode = this.getRequiredTranslatorBlockAtSocket(1).toCode();
+		String funcName = this.getRequiredTranslatorBlockAtSocket(2).toCode();
+		
+		String ret = "";
+//		 attachInterrupt(digitalPinToInterrupt(2),attachInterrupt_fun_2,CHANGE);
+
+		ret += "attachInterrupt(digitalPinToInterrupt(" 
+		+ pin + "),"
+		+ funcName.replace("\"","") + ","
+		+ mode + ");\n";
+
+//		translator.addDefinitionCommand(ultraSonicFunction);
+
+		return ret;
+	}
+	
+}
